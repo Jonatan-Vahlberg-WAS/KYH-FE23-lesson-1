18081f8d900fcb1d7d74fdbcf317ae3bf6e8393f
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-08-19 14:17:02 +0200

    add type declaration starter code

--- dist/types.js
+++ dist/types.js
@@ -1,1 +1,25 @@
 "use strict";
+// Type decaleration
+function introduceStudent(name, age, isStudent, courses) {
+    let introduction = `Name: ${name}`;
+    introduction += `\nAge: ${age}`;
+    introduction += `\nIs student: ${isStudent ? "Yes" : "No"}`;
+    introduction += `\nCourses: ${courses.join(", ")}`;
+    return introduction;
+}
+const student1 = {
+    fullName: "Jonatan V",
+    dateOfBirth: new Date("1996-12-26 18:00"),
+    courses: [
+        "FE23 - Typescript",
+        "FE23 - AM2",
+    ],
+    expelled: false,
+};
+function listStudents(students) {
+    students.map(student => {
+        console.log(student.fullName);
+    });
+}
+listStudents([student1]);
+// introduceAllStudents except expelled
---
+++ src/types.ts
@@ -0,0 +1,39 @@
+// Type decaleration
+
+type Student = {
+    fullName: string;
+    dateOfBirth: Date;
+    courses: string[];
+    expelled: boolean;
+}
+
+function introduceStudent(name: string, age: number, isStudent: boolean, courses: string[]): string {
+    let introduction = `Name: ${name}`
+    introduction += `\nAge: ${age}`
+    introduction += `\nIs student: ${isStudent ? "Yes" : "No"}`
+    introduction += `\nCourses: ${courses.join(", ")}`
+    return introduction
+}
+
+
+
+const student1: Student = {
+    fullName: "Jonatan V",
+    dateOfBirth: new Date("1996-12-26 18:00"),
+    courses: [
+        "FE23 - Typescript",
+        "FE23 - AM2",
+    ],
+    expelled: false,
+}
+
+function listStudents(students: Student[]) {
+    students.map(student => {
+        console.log(student.fullName)
+    })
+}
+
+listStudents([student1])
+
+
+// introduceAllStudents except expelled
